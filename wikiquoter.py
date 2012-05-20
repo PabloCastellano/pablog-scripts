ff195398a5d4359e4af6620213d6bcbd257637e3
Pablo Castellano <pablo@anche.no>
2012-05-20 13:05:31 +0200

    [wikiquoter] Version 0.7 - Añadidos "La Verdad" y "20 Minutos"

--- wikiquoter.py
+++ wikiquoter.py
@@ -24,14 +24,16 @@
 # http://www.europapress.es/economia/noticia-alonso-pp-advierte-sindicatos-manifestaciones-no-crean-empleos-les-invita-trabajar-serio-20120211141205.htm
 # http://www.lasprovincias.es/20120516/mas-actualidad/politica/corts-financiacion-local-201205161333.html
 # http://www.elcorreo.com/vizcaya/20120516/local/azkuna-comercio-tiene-espabilar-201205161922.html
+# http://www.laverdad.es/murcia/20120519/local/region/pedro-chico-tenemos-conseguir-201205191141.html
+# http://www.20minutos.es/noticia/1459072/0/de-guindos/nacionalizacion-bankia/futuro-privatizacion/
 
 __author__ = "Pablo Castellano <pablo@anche.no>"
 __license__ = "GNU GPLv3+"
-__version__ = 0.6
-__date__ = "17/05/2012 #caixarolada day!"
+__version__ = 0.7
+__date__ = "20/05/2012 #LaCaixaEsMordor"
 
 
-SUPPORTED_SITES = {'abc':'Diario ABC', 'publico':'Diario Público', 'economista':'El Economista', 'cs':'Cadena Ser', 'ep':'Europa Press', 'lp':'Las Provincias', 'correo':'El Correo'}
+SUPPORTED_SITES = {'abc':'Diario ABC', 'publico':'Diario Público', 'economista':'El Economista', 'cs':'Cadena Ser', 'ep':'Europa Press', 'lp':'Las Provincias', 'correo':'El Correo', 'lv':'La Verdad', '20m':'20 Minutos'}
 
 import re
 import sys
@@ -85,6 +87,12 @@ def getCite(url, t):
 	elif t == 'correo':
 		titulo = re.search('<title>(.*)</title>', ll).group(1)[:-11]
 		fecha = re.search('<div class="date">(.*) - <span>', ll).group(1)
+	elif t == 'lv':
+		titulo = re.search('<title>(.*)</title>', ll).group(1)[:-10]
+		fecha = re.search('<div class="date">(.*) - <span>', ll).group(1)
+	elif t == '20m':
+		titulo = re.search('<title>(.*)</title>', ll).group(1)[:-15]
+		fecha = re.search('<li class="author">.* (.*) - .*', ll).group(1);
 
 	print "* \"-frase-\"\n** Fuente: [%s %s], %s, %s" %(url, titulo, SUPPORTED_SITES[t], fecha)
 
