0d79b3670d64c5ac578b6cc676f8f5738cce724d
Pablo Castellano <pablo@anche.no>
2013-02-13 13:56:17 +0100

    [wikiquoter] Release version 1.0
    * Python pep8
    * Use datetime and locale to show localized month name
    * Allow different format results
    * Debug mode (verbose)
    * UTF-8
    * Fixed minor bugs

--- wikiquoter.py
+++ wikiquoter.py
@@ -29,16 +29,28 @@
 
 __author__ = "Pablo Castellano <pablo@anche.no>"
 __license__ = "GNU GPLv3+"
-__version__ = 0.9
-__date__ = "19/01/2013"
+__version__ = 1.0
+__date__ = "13/02/2013"
 __miscdate__ = "20/05/2012 #LaCaixaEsMordor"
 
 
+SUPPORTED_STYLES = ('15mpedia', 'eswikiquote')
 SUPPORTED_SITES = {'abc': 'Diario ABC', 'publico': 'Diario Público', 'economista': 'El Economista', 'cs': 'Cadena Ser', 'ep': 'Europa Press', 'lp': 'Las Provincias', 'correo': 'El Correo', 'lv': 'La Verdad', '20m': '20 Minutos'}
+STYLE = '15mpedia'
+DEBUG = False
 
+import datetime
+import locale
 import re
 import sys
 import urllib
+
+locale.setlocale(locale.LC_ALL, '')
+
+
+def print_debug(text):
+    if DEBUG:
+        print text
 
 
 def guessType(url):
@@ -54,7 +66,7 @@ def guessType(url):
         return 'ep'
     elif url.startswith('http://www.lasprovincias.es'):
         return 'lp'
-    elif url.startswith('http://www.elcorreo.es'):
+    elif url.startswith('http://www.elcorreo.com'):
         return 'correo'
     elif url.startswith('http://www.laverdad.es'):
         return 'lv'
@@ -64,13 +76,27 @@ def guessType(url):
         return None
 
 
+def formatResult(url, titulo, site, fecha):
+    global STYLE
+    if STYLE not in SUPPORTED_STYLES:
+        print 'WARNING: STYLE "%s" not valid. Using eswikiquote by default'
+        STYLE = 'eswikiquote'
+
+    if STYLE == '15mpedia':
+        result = "* [%s %s] (%s)" % (url, titulo, fecha.strftime('%d de %B de %Y'))
+    elif STYLE == 'eswikiquote':
+        result = "* \"-frase-\"\n** Fuente: [%s %s], %s, %s" % (url, titulo, site, fecha.strftime('%d de %B de %Y'))
+
+    return result.encode('utf-8')
+
+
 def getCite(url, t=None):
     if t is not None and t not in SUPPORTED_SITES.keys():
         print "Unsupported site. Please choose one from:"
         print SUPPORTED_SITES.keys()
         return
 
-    if not (url.startswith("http://") or url.startswith("www")):
+    if not (url.startswith("http://") or url.startswith('https://') or url.startswith("www")):
         print "URL looks wrong :?"
         sys.exit(1)
 
@@ -81,7 +107,7 @@ def getCite(url, t=None):
             print SUPPORTED_SITES.keys()
             sys.exit(1)
         else:
-            print 'Guessing url type... %s (%s)' % (SUPPORTED_SITES[t], t)
+            print_debug('Guessing url type... %s (%s)' % (SUPPORTED_SITES[t], t))
 
     f = urllib.urlopen(url)
 
@@ -91,9 +117,11 @@ def getCite(url, t=None):
     if t == 'abc':
         titulo = re.search('<title>(.*)</title>', ll).group(1).decode('latin-1')
         fecha = re.search('<div class="date">D&iacute;a (.*)( - <span>|</div>)', ll).group(1)
+        fecha = datetime.datetime.strptime(fecha, '%d/%m/%Y - <span>%H.%Mh</span>')
     elif t == 'publico':
         titulo = re.search('<title>(.*)</title>', ll).group(1)
         fecha = re.search('<span class="fecha">(.*)</span>', ll).group(1)
+        fecha = datetime.datetime.strptime(fecha, '%d/%m/%Y %H:%M')
 #       fecha_act = re.search('Actualizado: <span class="fecha">(.*)</span>', ll).group(1)
     elif t == 'economista':
         titulo = re.search('<title>(.*)</title>', ll).group(1)
@@ -101,42 +129,51 @@ def getCite(url, t=None):
             fecha = re.search('<div class="f-fecha">(.*) -', ll).group(1)
         except:
             fecha = re.search('<small>EcoDiario.es \| (.*) - .*<span', ll).group(1)
+        fecha = datetime.datetime.strptime(fecha, '%d/%m/%Y')
     elif t == 'cs':
-        titulo = re.search('<title>(.*) \| Sonido', ll).group(1).decode('latin-1')
+        titulo = re.search('<title>(.*) \| (Noticia|Sonido)', ll).group(1).decode('latin-1')
         if url.endswith('/'):  # No viene la fecha!! :?
             fecha = url.split('/')[-3][:8]
         else:
             fecha = url.split('/')[-2][:8]
+        fecha = datetime.datetime.strptime(fecha, '%Y%m%d')
     elif t == 'ep':
         titulo = re.search('<title>\r\n\t(.*)\r\n</title>', ll).group(1).decode('latin-1')
         import HTMLParser
         htmlparser = HTMLParser.HTMLParser()
         titulo = htmlparser.unescape(titulo)
         fecha = url.split('/')[-1].split('-')[-1][:8]
+        fecha = datetime.datetime.strptime(fecha, '%Y%m%d')
     elif t == 'lp':
     #   titulo = re.search('<div class="mpdato">(.*)</div>', ll).group(1)
-        titulo = re.search('<title>(.*)</title>', ll).group(1)[:-15]
+        titulo = re.search('<title>(.*)</title>', ll).group(1)[:-15].decode('latin-1')
         fecha = re.search('<div class="date">(.*) - <span>', ll).group(1)
+        fecha = datetime.datetime.strptime(fecha, '%d.%m.%y')
     elif t == 'correo':
         titulo = re.search('<title>(.*)</title>', ll).group(1)[:-11]
         fecha = re.search('<div class="date">(.*) - <span>', ll).group(1)
+        fecha = datetime.datetime.strptime(fecha, '%d.%m.%y')
     elif t == 'lv':
-        titulo = re.search('<title>(.*)</title>', ll).group(1)[:-10]
+        titulo = re.search('<title>(.*)</title>', ll).group(1)[:-10].decode('latin-1')
         fecha = re.search('<div class="date">(.*) - <span>', ll).group(1)
+        fecha = datetime.datetime.strptime(fecha, '%d.%m.%y')
     elif t == '20m':
-        titulo = re.search('<title>(.*)</title>', ll).group(1)[:-15]
+        titulo = re.search('<title>(.*)</title>', ll).group(1)[:-15].decode('latin-1')
         try:
             fecha = re.search('<li class="author">.* (.*) - .*', ll).group(1)
         except:
             fecha = re.search('<li class="author">.* (.*)</li>', ll).group(1)
+        fecha = datetime.datetime.strptime(fecha, '%d.%m.%Y')
 
-    print "* \"-frase-\"\n** Fuente: [%s %s], %s, %s" % (url, titulo, SUPPORTED_SITES[t], fecha)
+    result = formatResult(url, titulo, SUPPORTED_SITES[t], fecha)
+    print result
+    return result
 
 
 if __name__ == "__main__":
 
     print "Wikiquoter - Extrae citas directas para es.wikiquote.org"
-    print "Copyright (C) 2011-2012 Pablo Castellano"
+    print "Copyright (C) 2011-2013 Pablo Castellano"
     print "This program comes with ABSOLUTELY NO WARRANTY."
     print "This is free software, and you are welcome to redistribute it under certain conditions."
     print
