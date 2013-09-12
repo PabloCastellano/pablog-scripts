63d21b2d8ab36973288db530f9dc7f999f56cc48
Pablo Castellano <pablo@anche.no>
2013-09-12 22:30:13 +0200

    [convert-to-xvid] Removed -aspect 4:3

--- convert-to-xvid.sh
+++ convert-to-xvid.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Pablo Castellano <pablo@anche.no> 2013
 # sudo apt-get install libxvidcore4 libavcodec-extra-53 libav-tools
+# 1440x1080 / 2.25 = 640x480 ;_)
+# Don't force aspect ratio 4:3 as it will shrink some videos
 
 echo "Convert video files to AVI (XviD + MP3) using avconv"
 echo "$0 [files...]"
@@ -11,5 +13,9 @@ for file in "$@"; do
     echo "Converting file... $file ($newfile)"
     echo "------------------------"
     echo
-    avconv -i "$file" -f avi -r 29.97 -vcodec libxvid -vtag XVID -vf scale=640:480 -aspect 4:3 -maxrate 1800k -b 1500k -qmin 3 -qmax 5 -bufsize 4096 -mbd 2 -bf 2 -trellis 1 -flags +aic -cmp 2 -subcmp 2 -g 300 -acodec libmp3lame -ar 48000 -ab 128k -ac 2 "$newfile"
+    avconv -i "$file" -f avi -r 29.97 -vcodec libxvid -vtag XVID -vf scale=640:480 -maxrate 1800k -b 1500k -qmin 3 -qmax 5 -bufsize 4096 -mbd 2 -bf 2 -trellis 1 -flags +aic -cmp 2 -subcmp 2 -g 300 -acodec libmp3lame -ar 48000 -ab 128k -ac 2 -threads 0 "$newfile"
+    # avconv -i "$file" -f avi -r 29.97 -vcodec libxvid -vtag XVID -vf scale=640:480 -aspect 4:3 -maxrate 1800k -b 1500k -qmin 3 -qmax 5 -bufsize 4096 -mbd 2 -bf 2 -trellis 1 -flags +aic -cmp 2 -subcmp 2 -g 300 -acodec libmp3lame -ar 48000 -ab 128k -ac 2 "$newfile"
+    # -aspect 4:3 jode algunos videos. QUITAR?
+    # scale se podria cambiar tb. Hacer pruebas de tamaño final de archivos
+    # wish: Use multiple cores/threads
 done
